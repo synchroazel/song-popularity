9b9805b7fbf6c1684fd0ba7653d88eee1d0b0054
atanasova16 <m.atanasovaa@yahoo.com>
2022-06-01 17:39:12 +0300

    MQTT publishing

---
+++ mqtt_try.py
@@ -0,0 +1,10 @@
+import paho.mqtt.client as mqtt
+
+broker = 
+
+client = mqtt.Client("ME")
+client.username_pw_set("Publisher1", password="WINXCLUB")
+client.connect("broker.hivemq.com")
+client.subscribe("Songs_POP")
+client.publish("Songs_ROCK", "Try number 3")
+client.publish("Songs_POP", "Try pop")
