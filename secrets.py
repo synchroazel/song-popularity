e548d38913b2427e4e1c419714f19d4338865012
synchroazel <antonio.padalino.me@gmail.com>
2022-06-03 08:28:38 +0200

    added secrets.py

---
+++ secrets.py
@@ -0,0 +1,11 @@
+# YouTube API
+yt_api_key = 'AIzaSyBrwNTSinLqhT7p-cOlVkzyUhkc5JAT9qM'
+yt_client = '168166509565-kl2ki885efpiff8h64v8c18npguq3ijj.apps.googleusercontent.com'
+yt_client_secret = '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'
+
+# Spotify API
+sp_client = 'cc9b11f2d73045ac954aa575677feba5'
+sp_client_secret = '2925a27703d3423b93be7082192e4bb9'
+
+sp_client_id_bak = '7377d5af32ef4bd0ab9e118d061486e9'
+sp_client_secret = 'c4b6f4742c0e48c0a4b06153c76366ce'
