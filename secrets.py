9da3476771388e721a59db881e665837ab15cc5c
Antonio Padalino <antonio.padalino.me@gmail.com>
2022-12-27 08:13:49 +0100

    added redacted secrets.py

---
+++ secrets.py
@@ -0,0 +1,11 @@
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# YouTube API
+yt_api_key = 'REDACTED'
+yt_client = 'REDACTED'
+yt_client_secret = 'REDACTED'
+
+# Spotify API
+sp_credentials = SpotifyClientCredentials(client_id='REDACTED',
+                                          client_secret='REDACTED')
+
